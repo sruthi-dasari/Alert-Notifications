28e96e2a123b6f8144faf89a19501da775d7744a
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-02-25 17:02:08 +0530

    Update index.js

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,34 +1,43 @@
 import './index.css'
 import {AiFillCheckCircle} from 'react-icons/ai'
-import {GrFormClose} from 'react-icons/gr'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import {MdWarning, MdInfo} from 'react-icons/md'
+
 import Notification from '../Notification'
 
 const AlertNotifications = () => (
   <div className="alert-notifications-container">
     <h1 className="main-heading">Alert Notifications</h1>
     <Notification>
-      <AiFillCheckCircle className="icon-img" />
-      <p>Success</p>
-      <p>You can access all the files the folder</p>
-      <GrFormClose />
+      <AiFillCheckCircle className="icon-img success-icon" />
+      <h1 className="notification-title success-title">Success</h1>
+      <p className="notification-description">
+        You can access all the files in the folder
+      </p>
     </Notification>
     <Notification>
-      <AiFillCheckCircle className="icon-img" />
-      <p>Error</p>
-      <p>Sorry, you are not authorized to have access to delete the file</p>
-      <GrFormClose />
+      <RiErrorWarningFill className="icon-img error-icon" />
+      <h1 className="notification-title error-title">Error</h1>
+      <p className="notification-description">
+        Sorry, you are not authorized to have access to delete the file
+      </p>
+      {/* <GrFormClose className="cross-icon" /> */}
     </Notification>
     <Notification>
-      <AiFillCheckCircle className="icon-img" />
-      <p>Warning</p>
-      <p>Viewers of this file can see comments and suggestions</p>
-      <GrFormClose />
+      <MdWarning className="icon-img warning-icon" />
+      <h1 className="notification-title warning-title">Warning</h1>
+      <p className="notification-description">
+        Viewers of this file can see comments and suggestions
+      </p>
+      {/* <GrFormClose className="cross-icon" /> */}
     </Notification>
     <Notification>
-      <AiFillCheckCircle className="icon-img" />
-      <p>Info</p>
-      <p>Anyone on the Internet can view these files</p>
-      <GrFormClose />
+      <MdInfo className="icon-img info-icon" />
+      <h1 className="notification-title info-title">Info</h1>
+      <p className="notification-description">
+        Anyone on the Internet can view these files
+      </p>
+      {/* <GrFormClose className="cross-icon" /> */}
     </Notification>
   </div>
 )
